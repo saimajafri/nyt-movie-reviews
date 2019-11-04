b5c9cf18567e8c05a966f172412122d22661b551
Saima Jafri <saimajafri88@gmail.com>
2019-11-04 11:50:56 -0500

    update movie display attributes per api response

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Movie.css";
 
 const DEFAULT_PLACEHOLDER_IMAGE =
   "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg";
@@ -6,18 +7,19 @@ const DEFAULT_PLACEHOLDER_IMAGE =
 
 const Movie = ({ movie }) => {
   const poster =
-    movie.Poster === "N/A" ? DEFAULT_PLACEHOLDER_IMAGE : movie.Poster;
+    movie.multimedia.src === "N/A" ? DEFAULT_PLACEHOLDER_IMAGE : movie.multimedia.src;
+
   return (
     <div className="movie">
-      <h2>{movie.Title}</h2>
+      <h2>{movie.display_title}</h2>
       <div>
         <img
           width="200"
-          alt={`The movie titled: ${movie.Title}`}
+          alt={`The movie titled: ${movie.display_title}`}
           src={poster}
         />
       </div>
-      <p>({movie.Year})</p>
+      <p>({movie.opening_date})</p>
     </div>
   );
 };
